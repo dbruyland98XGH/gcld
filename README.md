2e0a1ab677305cf79d4e86b355161d13fc1fc8a5
dbruyland98XGIT <dbruyland98@hotmail.com>
2015-03-20 17:14:39 +0000

    rev12 20mrch

--- README.md
+++ README.md
@@ -1,26 +1,29 @@
-#Author: Dirk Bruyland
+# Project repository for Course: get and clean data
+
+## Author: Dirk Bruyland
+rev.11
+
  This is the Readme.md file for
-# the project repo for Course: get and clean data
- as described on this site:
+ the course project as described on this site:
+[https://class.coursera.org/getdata-012]
 
-
-Content
-=======
+#Content
+---
 The following files are in this repo:
 
-1. The RAW DATA 
+## 1 The RAW DATA 
 This is a ZIPped file (bzip2) fetched as is on March 03 2015, 13:49
 This file was fetched from the site:
-"https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip  "
+["https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip  "]
 
-2. Readme.md
+## 2 Readme.md
 This is this file
 
-3.run_analysis.R
+## 3 run_analysis.R
 This is the R script that produces the tidy data
 from the raw data, and stores the result in Tidy.txt.
 
-4.run_analysisH.csv
+## 4 run_analysisH.csv
 This is a helper file for the R script
 It is a modified copy of features.txt and has
 3 columnns. It contains per row :
@@ -28,20 +31,20 @@ a)the reference number to the feature,
 b)the modified name of the feature, and whether it will
 c) be included in the tidy data(1) or not(0)
 
-5. Tidy.txt
+## 5 Tidy.txt
 This is the output of running the R script 
 run_analysis.R
-It is a text file.
+It is a text file, made with write.table() and row.names=FALSE.
 
-6. CODEBOOK.md
+## 6 CODEBOOK.md
 This file explains the variables in tidy.txt
 This file also explains the design.
 
 
 
 
-Instruction
-============
+# Instruction
+---
 
 The raw data should be installed and unzipped in a /data
 subdirectory/folder from the working directory/folder , 
@@ -54,7 +57,6 @@ The resulting tidy data is provided by the script
 as a .txt fomatted file
 called tidy.txt in the working directory. 
 
-If newer raw data is provided or the user prefers to fetch the raw data
-the relevant download.file comments in run_analysis.R
-can be removed for that.
+If no data directory is there, there will
+one be created and the rawdata is fetched from the original site.
 
--- run_analysis.R
+++ run_analysis.R
@@ -12,3 +12,94 @@
 #
 #  5  From the data set in step 4, creates a second, 
 #  independent tidy data set with the average of each variable for each activity and each subject.
+#
+#
+#
+#
+library(dplyr)
+
+# check if rawdata present if not load it from original source
+if(!file.exists("./data")){
+dir.create("./data")
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip "
+download.file(fileUrl,destfile="./data/rawdata.zip")
+}
+
+#
+# read in the datafiles
+#
+X_test<-read.csv("./data/UCI HAR Dataset/test/X_test.txt",header=FALSE,stringsAsFactor=FALSE,sep="")
+y_test<-read.csv("./data/UCI HAR Dataset/test/y_test.txt",header=FALSE,stringsAsFactors=FALSE,sep="")
+s_test<-read.csv("./data/UCI HAR Dataset/test/subject_test.txt",header=FALSE)
+
+X_train<-read.csv("./data/UCI HAR Dataset/train/X_train.txt",header=FALSE,stringsAsFactor=FALSE,sep="")
+y_train<-read.csv("./data/UCI HAR Dataset/train/y_train.txt",header=FALSE,stringsAsFactors=FALSE,sep="")
+s_train<-read.csv("./data/UCI HAR Dataset/train/subject_train.txt",header=FALSE)
+
+RAH<-read.csv("run_analysisH.csv",header=FALSE,stringsAsFactor=FALSE)
+
+#
+# merge test and train sets
+#
+X_train<-cbind(X_train,s_train,y_train)
+X_test<-cbind(X_test,s_test,y_test)
+X_tot<-rbind(X_test,X_train)
+#
+# names and labelling of measurements
+# add 2 rows to the helper data structure RAH for subject and training
+# RAH is the helper data structure and is a data frame with 3 columsn and 563 rows
+# column 2 of RAH are all the names used  for column names
+# column 3 of RAH decides whether a feature/name is going to be used(1) or not(0)
+# only 86 features are selected(=1) and the subject and training are selected for total=88 
+#
+RAH<-rbind(RAH,c(562,"subject",1),c(563,"training",1))
+names_tot<-RAH[,2]
+names(X_tot)<-names_tot
+#
+#subset/extract measurements with mean and std
+#colsel_tot is a logical vecor made from column 3 of RAH
+#
+colsel_tot<-RAH[,3]==1
+X_sel<-X_tot[,colsel_tot]
+#
+# save the 88 relevant names (all 86 selected std/mean feature names plus "subject" and "training")
+#
+savednames<-names(X_sel)
+#
+# split X_sel according to 2 factors = training/activity and subject/individual
+# we will have a list XL  of 180 elements one element for 30 subjects/individuals doing 6 activities/trainings
+# each element of this list XL is a dataframe with n observations of 88 variables/features
+#
+XL<-split(X_sel,list(X_sel$subject,X_sel$training),drop=TRUE)
+#
+# take the column means of each element=dataframe of XL
+# XLL is a list of 180 vectors. each element of this list XLL is a numeric vector with 88 "means" which are the columnmeans
+#
+XLL<-lapply(XL,function(x) colMeans(x))
+#
+# reformat the resulting list XLL
+# All the column means are shifted in one vector V of length=88*180
+# XLL[[i]] is one such vector with 88 means.
+#
+V<-numeric()
+for (i in seq_along(XLL)) {V<-c(V,XLL[[i]])}
+#
+#Put the tidied up data in a matrix Vm
+#Vm has 180 rows and 88 columns and is a numeric matrix
+#
+#give the matrix Vm, the tidy data, and the appropriate column names.
+#
+Vm<-matrix(V,ncol=88,byrow=TRUE)
+colnames(Vm)<-savednames
+#
+# make data frame, and name and factor the activities
+#
+Vm<-data.frame(Vm)
+Vm[,88]<-factor(Vm[,88],labels=c("WALKING","WALKINGUPSTAIRS","WALKINGDOWNSTAIRS","SITTING","STANDING","LAYING"))
+#
+# write the tidy data in file tidy.txt
+#
+write.table(Vm,file="tidy.txt",row.names=FALSE)
+
+
+
